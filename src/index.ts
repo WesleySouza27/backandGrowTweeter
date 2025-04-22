b4fd184e67c9455d76c9b380b4093e64fb16ed13
Wesley Souza <wesleysilvanfl@gmail.com>
2025-04-21 23:35:59 -0300

    adicionado vercel.json e ajustes no index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,4 @@ declare global {
   }
 }
 
-const PORT = process.env.PORT || 3030;
-
-async function startServer() {
-  try {
-    // Testa a conexão com o banco de dados
-    await prismaClient.$connect();
-    console.log('Conexão com o banco de dados estabelecida.');
-
-    // Inicia o servidor
-    app.listen(PORT, () => {
-      console.log(`Servidor a correr na porta ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Erro ao conectar ao banco de dados:', error);
-    process.exit(1); // Encerra o processo se a conexão falhar
-  }
-}
-
-startServer();
+export default app;
